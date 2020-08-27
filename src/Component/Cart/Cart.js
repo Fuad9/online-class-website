7048453b0fdfafd1afdfd6af589d5225c4715663
Fuad9 <alfuad8548@gmail.com>
2020-08-27 21:45:02 +0600

    online-class-website done

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -3,10 +3,20 @@ import React from "react";
 const Cart = (props) => {
   let totalPrice = 0;
   totalPrice = props.cart.reduce((acc, current) => acc + current.price, 0);
+
+  const formatNumber = (num) => {
+    let precision = num.toFixed(2);
+    return Number(precision);
+  };
+
+  const styles = {
+    color: "green",
+  };
+
   return (
     <div>
-      <h4>Course Purchased: {props.cart.length}</h4>
-      <h4>Total Price: {totalPrice}</h4>
+      <h4 style={styles}>Course Purchased: {props.cart.length}</h4>
+      <h4 style={styles}>Total Price: ${formatNumber(totalPrice)}</h4>
     </div>
   );
 };
